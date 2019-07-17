a7b8c6153379385438aec42e9565b6950d69c1f6
WhiteLie1 <1136462446@qq.com>
2019-07-18 00:00:26 +0800

    camera调试代码，修复了无法打开相册的功能
    点击皮肤测试，跳转到camera方法，实现拍照功能

--- pages/camera/camera.js
+++ pages/camera/camera.js
@@ -7,7 +7,37 @@ Page({
     //图片路径的设置
     tempFilePaths:'' 
   },
-  
+  chooseimage: function () {
+    var that = this;
+    wx.showActionSheet({
+      itemList: ['从相册中选择'], //, '拍照'
+      itemColor: "#CED63A",
+      success: function (res) {
+        if (!res.cancel) {
+          if (res.tapIndex == 0) {
+            that.chooseWxImage('album')
+          } else if (res.tapIndex == 1) {
+            that.chooseWxImage('camera')
+          }
+        }
+      }
+    })
+
+  },
+
+  chooseWxImage: function (type) {
+    var that = this;
+    wx.chooseImage({
+      sizeType: ['original', 'compressed'],
+      sourceType: [type],
+      success: function (res) {
+        console.log(res);
+        that.setData({
+          tempFilePaths: res.tempFilePaths[0],
+        })
+      }
+    })
+  },
 
   /**
    * 生命周期函数--监听页面加载
@@ -60,7 +90,7 @@ Page({
     
   },
 
-  /**
+  /**   
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
